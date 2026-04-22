989518b625872e2a4d3ade41969a13d41f61fb8b
vatsalpatelvision-ops <vatsalpatel.vision@gmail.com>
2026-04-22 18:28:29 +0530

    delete query

--- MyProject/demo project/main.py
+++ MyProject/demo project/main.py
@@ -6,7 +6,7 @@ Base.metadata.create_all(bind=engine)
 def create_user():
     db = SessionLocal()
     try:
-        new_user = User(name="honey" , email="honey@gmail.com")
+        new_user = User(name="harsh" , email="rajput@gmail.com")
         # new_user = User(name="vatsal" , email="vatsal@gmail.com")
 
         db.add(new_user)
@@ -50,7 +50,40 @@ def get_user_by_id(id):
     finally:
         db.close()
 
+def update_user_name(name , id):
+    db = SessionLocal()
+
+    try:
+        user = db.query(User).filter(User.id == id).first()
+
+        user.name = name
+        db.commit()
+        db.refresh(user)
+        print(f"user {id} updated with {user.name}")    
+    except Exception as e:
+        print("User not found")
+    finally:
+        db.close()
+
+
+def delete_user_by_id(id):
+    db = SessionLocal()
+    try:
+        user = db.query(User).filter(User.id == id).first()
+
+        db.delete(user)
+        db.commit()
+        print("User Deleted")
+    except Exception as e:
+        print("User not found : ")
+    finally:
+        db.close()
+
+
+
 if __name__ == "__main__":
     # create_user()
     # get_users()
-    get_user_by_id(2)
+    # get_user_by_id(2)
+    # update_user_name("chintu",2)
+    delete_user_by_id(3)
